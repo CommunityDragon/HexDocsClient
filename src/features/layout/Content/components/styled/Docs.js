d51b671fb98c5b2248ba2b4f4d11e0646d4b94f9
Ray <dev@hiray.me>
2020-03-15 03:05:22 +0000

    Added Docs.js

---
+++ src/features/layout/Content/components/styled/Docs.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+const Docs = styled.div`
+  hr {
+    border-top: 1px solid rgba(0,0,0,0.11);
+  }
+
+  .doc-author {
+    
+  }
+  .doc-name {
+    font-size: 1rem;
+  }
+  .doc-meta {
+    display: flex;
+    flex-direction: column;
+    color: rgba(0,0,0,0.33);
+    padding-bottom: 1vw;
+    font-size: 0.7rem;
+  }
+`
+
+export default Docs
