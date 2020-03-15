79c9dbcfd3bc53d489ae285eb204feb507cee6ee
Ray <dev@hiray.me>
2020-03-15 11:03:32 +0000

    fixed indentation

--- src/features/layout/Loading/components/styled/Loading.js
+++ src/features/layout/Loading/components/styled/Loading.js
@@ -1,33 +1,25 @@
 import styled from 'styled-components'
 
 const Loading = styled.div`
-     @keyframes wiggle {
-      0% {
-        opacity: 0;
-      }
-      100% {
-        opacity: 100%;
-      }
-     }
+  @keyframes wiggle {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 100%;
+    }
+  }
 
   width: 100%;
   height: 100%;
-  
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
   
   .load-img {
-    height: 50%;
-    width: auto;
-    animation-name: wiggle;
-    animation-duration: 2s;
-    animation-iteration-count: infinite;
-    animation-timing-function: ease-in-out;
-    filter: invert(100%);
-    display: flex;
-    justify-content: center;
-    align-content: center;
-    align-items: center;
+    width: 15%;
+    height: auto;
   }
 `
 
