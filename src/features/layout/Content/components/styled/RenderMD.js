00bdc92a82b6c13831ead3fec477cf667d10e14c
Ray <dev@hiray.me>
2020-03-15 03:14:34 +0000

    Added Content RenderMD.js styles

---
+++ src/features/layout/Content/components/styled/RenderMD.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+const RenderMD = styled.div`
+    padding: 1vh 0 2vh 0;
+    .categories {
+      .markdown-body {
+        box-sizing: border-box;
+        min-width: 200px;
+        max-width: 980px;
+        margin: 0 auto;
+        padding: 45px;
+        
+    }
+
+    @media (max-width: 767px) {
+      .markdown-body {
+        padding: 15px;
+      }
+    }
+  }
+`
+
+export default RenderMD
