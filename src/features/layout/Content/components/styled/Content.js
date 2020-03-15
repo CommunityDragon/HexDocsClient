4996354fb43ad75eb81541327b7f7724ae0ed429
Ray <dev@hiray.me>
2020-03-15 03:05:06 +0000

    Added Content.js

---
+++ src/features/layout/Content/components/styled/Content.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+const SubCategories = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap');
+  font-size: 1rem;
+  max-width: 1000px;
+  font-family: 'Inter', sans-serif;
+  .link {
+    transition-duration: 450ms;
+    cursor: pointer;
+    &:hover {
+      transition-duration: 350ms;
+      filter: invert(50%);
+    }
+  }
+  
+  .docs {
+  line-height: 150%;
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+export default SubCategories
