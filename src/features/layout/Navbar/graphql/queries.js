a5f172034eaf039f47f6fe22b43ef6d81eeda8bd
Ray <dev@hiray.me>
2020-04-17 22:04:11 +0100

    Separated Graphql queries

--- src/features/layout/Navbar/graphql/queries.js
+++ src/features/layout/Navbar/graphql/queries.js
@@ -3,18 +3,13 @@ import { gql } from 'apollo-boost'
 /**
  * fetches category data for the navbar
  */
-export const GET_CATEGORIES = gql`
-  query Categories {
-    getMenuItems {
-      name
-      id
-      iconUrl
-      slug
-      subcategories {
-        name
-        id
-        slug
-      }
+export const GET_MENU_ITEMS = gql`
+    query Menu_Items {
+        getMenuItems {
+            name
+            id
+            iconUrl
+            slug
+        }
     }
-  }
 `
