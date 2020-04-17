23565003779a152caee4087e921b583631f61fc4
Ray <dev@hiray.me>
2020-04-17 22:05:51 +0100

    Changed from pepega to sidebar

--- src/features/layout/Sidebar/components/SidebarStatic.jsx
+++ src/features/layout/Sidebar/components/SidebarStatic.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 /**
  * pepega static
  */
-const PepegaStatic = () => (
+const SidebarStatic = () => (
   <p>Test TWO</p>
 )
 
-export default PepegaStatic
+export default SidebarStatic
